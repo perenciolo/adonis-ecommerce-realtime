19343c737882a185dd37c73847f8083b580f7088
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-08-12 10:03:22 -0300

    Implement ProductController

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Product = use('App/Models/Product')
+
 /**
  * Resourceful controller for interacting with products
  */
@@ -15,9 +17,19 @@ class ProductController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const title = request.input('title')
+    const query = Product.query()
+
+    if (title) {
+      query.where('name', 'ILIKE', `%${title}%`)
+    }
+
+    const products = await query.paginate(pagination.page, pagination.limit)
+    return response.json(products)
+  }
 
   /**
    * Create/save a new product.
@@ -27,7 +39,22 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      const { name, description, price, image_id: imageId } = request.all()
+      const product = await Product.create({
+        name,
+        description,
+        price,
+        image_id: imageId
+      })
+      return response.status(201).json(product)
+    } catch (error) {
+      return response.status(400).json({
+        message: 'Error creating product with given data'
+      })
+    }
+  }
 
   /**
    * Display a single product.
@@ -36,9 +63,11 @@ class ProductController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+    return response.send(product)
+  }
 
   /**
    * Update product details.
@@ -48,7 +77,19 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+    try {
+      const { name, description, price, image_id: imageId } = request.all()
+      product.merge({ name, description, price, image_id: imageId })
+      await product.save()
+      return response.json(product)
+    } catch (error) {
+      return response.status(400).json({
+        message: 'Error updating product with given data'
+      })
+    }
+  }
 
   /**
    * Delete a product with id.
@@ -58,7 +99,17 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+    try {
+      await product.delete()
+      return response.status(204).json()
+    } catch (error) {
+      return response.status(500).json({
+        message: 'It was not possible to delete this product, please try again'
+      })
+    }
+  }
 }
 
 module.exports = ProductController
